37a5aef0ae4980e766140659fe27fd7468888483
BeerRobot <ashley.pemberton2113@gmail.com>
2017-02-04 14:53:22 +0000

    Added Resource class.

---
+++ GameJam2017_Source/Assets/Scripts/Resource.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource : MonoBehaviour
+{
+    public string itemName = "";
+    public int itemID = 0;
+
+}
