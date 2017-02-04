0f4bd97499a077d64567c8fd96501ec22ab20ef9
BeerRobot <ashley.pemberton2113@gmail.com>
2017-02-04 21:57:39 +0000

    Shop menus and navigation.

---
+++ GameJam2017_Source/Assets/Scripts/CraftingHUD.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class CraftingHUD : MonoBehaviour
+{
+    public Transform listParent;
+    public GameObject itemButton;
+    public List<GameObject> itemListButtons = new List<GameObject>();
+
+    void Start()
+    {
+        if (FindObjectOfType<Inventory>().items.Count > 0)
+        {
+            foreach (Item i in FindObjectOfType<Inventory>().items)
+            {
+                GameObject go = Instantiate(itemButton, transform.position, Quaternion.identity) as GameObject;
+                go.transform.parent = listParent;
+                go.transform.FindChild("Text").GetComponent<Text>().text = i.itemName;
+                itemListButtons.Add(go);
+            }
+
+            EventSystem.current.SetSelectedGameObject(itemListButtons[0], new BaseEventData(EventSystem.current));
+        }
+    }
+
+    void OnEnable()
+    {
+        if (itemListButtons.Count > 0)
+            EventSystem.current.SetSelectedGameObject(itemListButtons[0], new BaseEventData(EventSystem.current));
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ GameJam2017_Source/Assets/Scripts/ShopController.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public enum MenuState { Main, Sell, Craft }
+
+public class ShopController : MonoBehaviour
+{
+    public MenuState menuState;
+    public GameObject shopMenu;
+    public GameObject sellMenu;
+    public GameObject craftMenu;
+
+    public GameObject startingButton;
+
+    void Start()
+    {
+        SetShop();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("ButtonB"))
+        {
+            if (menuState == MenuState.Craft)
+            {
+                SetShop();
+            }
+
+            if (menuState == MenuState.Sell)
+            {
+                SetShop();
+            }
+        }
+    }
+
+    void SetShop()
+    {
+        sellMenu.SetActive(false);
+        craftMenu.SetActive(false);
+        shopMenu.SetActive(true);
+        EventSystem.current.SetSelectedGameObject(startingButton, new BaseEventData(EventSystem.current));
+        menuState = MenuState.Main;
+    }
+
+    public void SetCraft()
+    {
+        sellMenu.SetActive(false);
+        craftMenu.SetActive(true);
+        shopMenu.SetActive(false);
+        menuState = MenuState.Craft;
+    }
+
+    public void SetSell()
+    {
+        sellMenu.SetActive(true);
+        craftMenu.SetActive(false);
+        shopMenu.SetActive(false);
+        menuState = MenuState.Craft;
+    }
+
+}
