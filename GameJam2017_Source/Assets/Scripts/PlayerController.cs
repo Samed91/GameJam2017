de710c89a54d41e683cd7cbdb5a0bcb13ab6530e
BeerRobot <ashley.pemberton2113@gmail.com>
2017-02-04 02:20:27 +0000

    Loads of assets bruv.

---
+++ GameJam2017_Source/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [Range(0, 5)]
+    public float movementSpeed = 3f;
+    [Range(0, 5)]
+    public float lookSpeed = 3f;
+
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal") * (lookSpeed * Time.deltaTime);
+        float v = Input.GetAxis("Vertical") * (lookSpeed * Time.deltaTime);
+
+        transform.eulerAngles = new Vector3(transform.eulerAngles.x, Mathf.Atan2(h, v) * Mathf.Rad2Deg, transform.eulerAngles.z);
+        transform.Translate(transform.forward * (movementSpeed * Time.deltaTime));
+
+    }
+
+}
---
+++ GameJam2017_Source/Assets/Store Assets/Fortress/ReadMe.txt
@@ -0,0 +1,8 @@
+Create your own Fortress in your City with the Fortress Pack.
+The models can be used on Mobil or any other platform, it contains a Low Poly version of the structures.
+It also has Hand painted High Quality textures.
+You can create several different designs from the walls, main structure and several towers, that can stand on the ground or be attached to larger structures, as sown on the Screenshots.
+It uses only one Texture, a 2048x2048 TGA
+
+The Pack is grouped in a main folder named "Fortress Pack" and it has a Demo Scene, shown on the Screenshots, and the Prefabs folder, which contains all the elements.
+Thank you, and may it serve you well!
